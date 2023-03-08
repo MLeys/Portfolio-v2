2c0a82d56ac3a03e00aa3a606d7ff1d198ddb041
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-08 18:47:24 -0500

    BUILD: Path routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,21 @@
 import * as React from 'react';
 import ButtonUnstyled from '@mui/base/ButtonUnstyled';
-
+import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+
+import HomePage from './Pages/HomePage/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <ButtonUnstyled>Hello World</ButtonUnstyled>
+    <Routes>
+      <Route path="/" element={ <HomePage />} />
+      <Route path="/*" element={ <Navigate to='/' />} />
+    </Routes>
 
-
-
-
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
 
