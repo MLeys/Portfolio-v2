1f532d095f773ee9dab46e87cde571f05acb3782
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-09 13:22:18 -0500

    BUILD: Landing page, setup initial styling

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import './LandingPage.css'
+import {
+  Container,
+  Segment,
+  Grid,
+  Image
+
+} from 'semantic-ui-react';
+
 
 function LandingPage() {
   return (  
-    <h1>Landing PAge</h1>
+    <Container 
+      style={{
+        width: '100vw',
+       
+      }}
+      // as={Grid}
+      fluid={true}
+    >
+      First thing
+      <Image 
+        style={{
+          width: '100vw'
+        }}
+        fluid={true}
+        src='https://imgur.com/bUeqQX5.png'
+      
+      >
+
+
+      </Image>
+    </Container>
   );
 }
 
